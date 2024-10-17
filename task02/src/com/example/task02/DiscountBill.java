3461aa5c2ae1f990abe0174e79f9b143cc488bcc
Tomaso1420 <Artem82005@mail.ru>
2024-10-17 22:27:52 +0500

    Марциохо задание 2

---
+++ task02/src/com/example/task02/DiscountBill.java
@@ -0,0 +1,25 @@
+package com.example.task02;
+
+public class DiscountBill extends  Bill {
+    private Double Discount;
+
+    public DiscountBill(double discount){
+        this.Discount = discount;
+    }
+
+    public void setDiscount(double discount){
+        this.Discount = discount;
+    }
+    public String  getDiscount(){
+        return Discount * 100 + "%";
+    }
+    @Override
+    public long getPrice(){
+        return (long)(super.getPrice() * (1 - Discount));
+    }
+
+    public long getAbsDiscount(){
+        return super.getPrice() - this.getPrice();
+    }
+
+}
